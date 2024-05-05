8f9b63ea91f8f186b5fb2705cc04f496b703465c
Etheratic <136394417+Etheratic@users.noreply.github.com>
2024-05-06 01:16:47 +1000

    Monster Appearance

---
+++ Unity/Assets/Scripts/MonsterTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterTrigger : MonoBehaviour
+{
+
+    public GameObject monster;
+    public GameObject monsterSpawnPoint;
+
+    protected Transform spawn;
+    //
+
+    private void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        print("working");
+        SpawnMonster();
+    }
+
+    public void SpawnMonster()
+    {
+        monster.transform.position = monsterSpawnPoint.transform.position;
+    }
+}
